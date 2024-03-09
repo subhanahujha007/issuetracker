f7ebc4c638651279c7d4da467a50a2346502f9fc
subhanshu jha <subhanshujha001@gmail.com>
2024-03-09 11:17:06 +0530

    projects etup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import {AiFillBug} from "react-icons/ai"
+const Navbar = () => {
+    const currentpath=usePathname()
+    const link=[{
+        href:'/',
+        title:'Dashboard'
+    }
+    ,{
+href:'/issues',
+title:'issues',
+    }]
+  return (
+    <div className='flex flex-row items-center h-16 w-full gap-8 p-4 border-b  '>
+        <Link href="/">
+        <AiFillBug/>
+        </Link>
+       { link.map(links=>
+        <Link href={links.href}
+        key={links.href}
+        className={`${links.href===currentpath?'text-zinc-900':'text-zinc-100'}hover:text-zinc-800 transition-colors text-lg `}
+        >
+        {links.title}
+        </Link>
+        )}
+    </div>
+  )
+}
+
+export default Navbar
