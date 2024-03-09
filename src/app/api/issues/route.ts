055cba107126df6efce119d569ee88940221c71f
subhanshu jha <subhanshujha001@gmail.com>
2024-03-09 14:37:32 +0530

    Created issues

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -2,8 +2,8 @@ import { NextResponse,NextRequest } from "next/server";
 import {z} from "zod"
 import prisma from "../../../../prisma/prisma";
 const issueschema=z.object({
-    title:z.string().min(1).max(255),
-    description:z.string().min(1)
+    title:z.string().min(1,'Title is required').max(255),
+    description:z.string().min(1,'description is required')
 })
 export  async function POST(request:NextRequest) {
     const body=await request.json()
--- src/app/issues/newissues/page.tsx
+++ src/app/issues/newissues/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {useForm,Controller} from "react-hook-form"
-import {Button,TextArea,TextField} from "@radix-ui/themes"
+import {Button,Callout,TextArea,TextField} from "@radix-ui/themes"
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
@@ -13,19 +13,27 @@ interface issuesform{
 }
 const page = () => {
   const {register,control,handleSubmit}=useForm<issuesform>()
+ const [error,seterror]=useState("") 
   const route=useRouter()
-  return (
-    <form  className='max-w-xl space-y-3 m-6 ' onSubmit={handleSubmit(async(data)=>{
+  return (<div className='max-w-xl space-y-3 m-6 ' >
+    {
+      error &&
+      <Callout.Root color='red' className='mb-5'>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>
+    }
+    <form  onSubmit={handleSubmit(async(data)=>{
 try {
   await axios.post('/api/issues',data)
   route.push("/issues")
 } catch (error) {
+  seterror("An unexpected error occurred")
   console.error(error)
 }
 
     })}>
-<TextField.Root>
-  <TextField.Input placeholder='title' {...register('title')} />
+<TextField.Root className='mb-2'>
+  <TextField.Input  placeholder='title' {...register('title')} />
 </TextField.Root>
 <Controller
 name='description'
@@ -33,6 +41,7 @@ control={control}
 render={({field})=><SimpleMDE placeholder='description' {...field} />} />
 <Button>Submit issue</Button>
      </form >
+     </div>
   )
 }
 
