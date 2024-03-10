6e200208ac3a00416810606736d14585e9367541
subhanshu jha <subhanshujha001@gmail.com>
2024-03-10 16:42:05 +0530

    added ui components and created status and date section in the issue page

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button } from '@mui/material'
+import { Callout } from '@radix-ui/themes'
 import {
   Pagination,
   PaginationContent,
@@ -9,11 +10,28 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import axios from 'axios'
-import { totalmem } from 'os'
 type issuesform={
   createdAt : Date,
   description:string, 
@@ -46,11 +64,17 @@ getdata()
     ><Button variant="outlined">Create New Issues</Button></Link>
 
 {client ? (<><div className='max-w-[900px] m-auto border '>
+  <div className='border p-5 justify-between flex flex-row '><p>issues</p><p>status</p><p>createdAt</p></div>
+  <br />
   {
   
   currentitems.map((issue) => (
-        <div className='border-b p-4' key={issue.title}>
+        <div className='border-b p-4 flex flex-row justify-between' key={issue.title}>
           <h3>{issue.title}</h3>
+          <Callout.Root color={`${issue.status==='OPEN'?'green':(issue.status==="CLOSED"?'red':'blue')}`} className='mb-5'>
+        <Callout.Text>{issue.status}</Callout.Text>
+      </Callout.Root>
+          <h3>{issue.createdAt.toString().split('T')[0]}</h3>
         </div>
       ))}
       </div>
@@ -63,7 +87,34 @@ getdata()
       />
       </>):(<center>Loading</center>)
       }
-      
+      <div className=" p-4 flex justify-between w-full">
+      <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Who is the developer ?</AccordionTrigger>
+    <AccordionContent>
+     Hey its me <Link href="https://github.com/subhanahujha007">Subhanshu jha</Link>
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
+<AlertDialog>
+  <AlertDialogTrigger>Click here to know more</AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>WHY DID I CREATED THIS PROJECT</AlertDialogTitle>
+      <AlertDialogDescription>
+        THIS PROJECT IS MADE SO THAT YOU CAN LEARN HOW TO CREATE ISSUE <br/>
+        AND TO DISPLAY MY AWESOME CODEING SKILLS 😉
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogAction>oh ok now go</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+
+</div>
+
   </>
   )
 }
@@ -101,7 +152,7 @@ for(let i=1;i<= Math.ceil(totalitems/itemsperpage);i++ ){
       ))
     }
     <PaginationItem>
-      <PaginationNext onClick={()=>{if(currentpage < pagenow.length)setcurrentpage(currentpage +1 )}} />
+      <PaginationNext onClick={() => { if (currentpage < pagenow.length) setcurrentpage(currentpage + 1) } }  />
     </PaginationItem>
   </PaginationContent>
 </Pagination>)
