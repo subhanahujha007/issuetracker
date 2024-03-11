ddfc9da6c4c1bcc50017b52d30c26f5c8948d1b6
subhanshu jha <subhanshujha001@gmail.com>
2024-03-11 21:40:09 +0530

    added the id page

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,22 +1,50 @@
 "use client"
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
-const page = ({params}:any) => {
-    const [data,setdata]=useState()
-    useEffect(() => {
-        async function getdata(){
-          const response=await axios.get(`${process.env.NEXT_PUBLIC_domain}/api/issues?${params.id}=1`)
-          setdata(response.data)
-            console.log(response.data)
-        }
-      getdata()
-      
-      }, [])
-    return (
-    <div>{params.id}</div>
-  )
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Callout } from '@radix-ui/themes';
+type IssuesForm = {
+    createdAt: Date,
+    description: string,
+    id: number,
+    status: string,
+    title: string,
+    updatedAt: Date,
 }
 
-export default page
+export const Page = ({ params }: any) => {
+    const [data, setData] = useState<IssuesForm | null>(null); 
+    const [client,setclient]=useState<boolean>(false)
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_domain}/api/issues`);
+            console.log(response.data[5].id)
+            const filteredData = response.data.find((item: IssuesForm) => item.id === Number(params.id));
+            console.log(filteredData)
+            setData(filteredData); 
+        }
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {
+client ?(
+<center>Loading ... </center>
+):(<div className='flex flex-row'>
+   
+<div className='p-4 flex flex-col gap-5 min-w-[700px] max-w-xl'>
+<Callout.Root color={`${data?.status==='OPEN'?'green':(data?.status==="CLOSED"?'red':'blue')}`} className='mb-5 max-w-[70px] max-h-[50px] p-1 mt-3'>
+        <Callout.Text>{data?.status}</Callout.Text>
+      </Callout.Root>
+    <h1 className='border p-2'>{data?.title}</h1>
+    <p className='border p-2 min-h-[300px]'>{data?.description}</p>
+</div>
+
+</div>
+)
+            }
+        </div>
+    );
+};
+
+export default Page
